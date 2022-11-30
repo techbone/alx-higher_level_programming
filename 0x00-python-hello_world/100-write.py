092e4deff9889a7e316fce355465c321e4ce1f80
Musa Muhammad Etudaye <musaawwaletudaye@gmail.com>
2022-11-30 15:51:36 -0800

    tasksk 11

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -4,4 +4,5 @@ import os
 
 fd = sys.stderr.fileno()
 data = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
-os.write(fd, data.encode("utf-8"
+os.write(fd, data.encode("utf-8"))
+sys.exit(1)
